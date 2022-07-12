faf494c537f5db7e6075884320ce143c321a93e1
xRadne <32938476+xRadne@users.noreply.github.com>
2022-07-12 15:38:49 +0200

    Chat app

--- index.js
+++ index.js
@@ -7,10 +7,30 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+const users = {};
+
 io.on('connection', (socket) => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
+  socket.on('chat message', data => {
+    const { message } = JSON.parse(data);
+    io.emit('chat message', JSON.stringify({ message, senderId: socket.id }));
   });
+
+  socket.on('update profile', data => {
+    const { name } = data;
+    users[socket.id].name = name;
+    io.sockets.emit('users updated', users);
+  });
+
+  socket.on('disconnect', () => {
+    io.sockets.emit('user disconnected', socket.id);
+    delete users[socket.id];
+  });
+
+  users[socket.id] = {
+    name: 'Anonymous ' + '(' + Math.floor(Math.random() * 100) + ')',
+    color: '#' + Math.floor(Math.random() * 16777215).toString(16)
+  };
+  io.sockets.emit('users updated', users);
 });
 
 http.listen(port, () => {
